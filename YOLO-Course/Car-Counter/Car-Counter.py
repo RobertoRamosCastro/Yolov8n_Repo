289355b14ba7e8a7e22966a04d8641aba09db929
RobertoRamosCastro <r.ramos.2017@alumnos.urjc.es>
2023-03-31 22:02:10 +0200

    Collecting ID in each frame

--- YOLO-Course/Car-Counter/Car-Counter.py
+++ YOLO-Course/Car-Counter/Car-Counter.py
@@ -2,10 +2,11 @@ from ultralytics import YOLO
 import cv2
 import cvzone
 import math
+from sort import *  
 
-model = YOLO(r'D:\YOLO-Course\chapter5-runningYolo\YOLO-Weights\yolov8n.pt')
+model = YOLO(r'D:\Yolov8n_Repo\YOLO-Course\chapter5-runningYolo\YOLO-Weights\yolov8n.pt')
 
-cap = cv2.VideoCapture(r'D:\YOLO-Course\Videos\cars.mp4')
+cap = cv2.VideoCapture(r'D:\Yolov8n_Repo\YOLO-Course\Videos\cars.mp4')
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
@@ -19,13 +20,22 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "teddy bear", "hair drier", "toothbrush"
               ]
 
+mask = cv2.imread(r'D:\Yolov8n_Repo\YOLO-Course\Car-Counter\mascara.png')
+
+# Tracker
+tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+
 while True:
     ret, frame = cap.read()
 
     if not ret:
         break
 
+    imgRegion = cv2.bitwise_and(frame, mask) # operacion para extraer la parte que queremos con nuestra mascara
+
     results = model(frame, stream=True)
+    # Para inicializar nuestros tracker
+    detections=np.empty((0,5))
 
     for r in results:
         boxes = r.boxes
@@ -46,20 +56,38 @@ while True:
             if classNames[cls] == 'car' or classNames[cls] == 'truck' or classNames[cls]=='motorbike' or classNames[cls]=='bus':
                 if conf > 0.5:
                     # Mostrar bounding box
-                    cvzone.cornerRect(frame, bbox=(x1,y1,w,h), l=15, t=2)
+                    cvzone.cornerRect(frame, bbox=(x1,y1,w,h), l=15, t=2, rt=5)
                     # Mostrarlo en texto
-                    cvzone.putTextRect(frame,
-                                    f'{classNames[cls]} {conf:.2f}',
+                    '''cvzone.putTextRect(frame,
+                                    f'{classNames[cls]} {conf:.2f} {ID}',
+                                    (max(0,x1), max(35, y1)),
+                                    scale=1,
+                                    thickness=1,
+                                    offset=3) '''
+                    # adding new detections in each loop
+                    currentArray = np.array([x1,y1,x2,y2,conf])
+                    detections = np.vstack((detections, currentArray))
+
+
+    # update with a list of detections
+    resultTracker = tracker.update(detections)
+
+    for result in resultTracker:
+        x1,y1,x2,y2,id = result
+        x1,y1,x2,y2 = int(x1), int(y1), int(x2), int(y2)
+        print(result)
+        w, h = x2-x1, y2-y1
+        cvzone.cornerRect(frame, bbox=(x1,y1,w,h), l=15, t=2, rt=2, colorR=(255,0,0))
+        cvzone.putTextRect(frame,
+                                    f'{id}',
                                     (max(0,x1), max(35, y1)),
                                     scale=1,
                                     thickness=1,
                                     offset=3) 
-            else:
-                continue
 
-            
-
+    
     cv2.imshow("Frame", frame)
+    cv2.imshow("ImgRegion", imgRegion)
 
     # Esperar a que se presione la tecla 'q' para cerrar la ventana y 50 ms entre frames
     key = cv2.waitKey(0)
