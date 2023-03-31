1aeddee3577727344b0472f0ca700200e02b1653
RobertoRamosCastro <r.ramos.2017@alumnos.urjc.es>
2023-04-01 01:35:11 +0200

    Last thing to do is to separate classes

--- YOLO-Course/Car-Counter/Car-Counter.py
+++ YOLO-Course/Car-Counter/Car-Counter.py
@@ -4,21 +4,11 @@ import cvzone
 import math
 from sort import *  
 
-model = YOLO(r'D:\Yolov8n_Repo\YOLO-Course\chapter5-runningYolo\YOLO-Weights\yolov8n.pt')
+model = YOLO(r'D:\Yolov8n_Repo\YOLO-Course\chapter5-runningYolo\YOLO-Weights\yolov8l.pt')
 
 cap = cv2.VideoCapture(r'D:\Yolov8n_Repo\YOLO-Course\Videos\cars.mp4')
 
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
-              ]
+classNames = ["-", "-", "car", "motorbike", "-", "bus", "-", "truck"]
 
 mask = cv2.imread(r'D:\Yolov8n_Repo\YOLO-Course\Car-Counter\mascara.png')
 
@@ -89,11 +79,11 @@ while True:
         w, h = x2-x1, y2-y1
         cvzone.cornerRect(frame, bbox=(x1,y1,w,h), l=15, t=2, rt=2, colorR=(255, 0, 255))
         cvzone.putTextRect(frame,
-                                    f'{id} {classNames[cls]}',
-                                    (max(0,x1), max(35, y1)),
-                                    scale=1,
-                                    thickness=1,
-                                    offset=3) 
+                            f'{id} {classNames[cls]}',
+                            (max(0,x1), max(35, y1)),
+                            scale=1,
+                            thickness=1,
+                            offset=3) 
         
         cx, cy =  x1+w//2, y1+h//2
         cv2.circle(frame, (cx,cy), 5, (255,0,255), cv2.FILLED)
@@ -102,15 +92,17 @@ while True:
             if id not in list_id_counted:
                 counter+=1
                 list_id_counted.append(id)
+                # Cuando detecta uno cambia de color
+                cv2.line(frame, (limits[0],limits[1]),(limits[2],limits[3]), (0,255,0), 5)
 
     # Mostar el conteo total
-    cvzone.putTextRect(frame, f'{counter}', (50,50)) 
+    cvzone.putTextRect(frame, f'Counts: {len(list_id_counted)}', (50,50)) 
     
     cv2.imshow("Frame", frame)
     # cv2.imshow("ImgRegion", imgRegion)
 
     # Esperar a que se presione la tecla 'q' para cerrar la ventana y 50 ms entre frames
-    key = cv2.waitKey(0)
+    key = cv2.waitKey(1)
     if key == ord('q'):
         cap.release()
         cv2.destroyAllWindows()
